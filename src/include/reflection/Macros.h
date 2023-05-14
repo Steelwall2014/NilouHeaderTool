d0930a4d12e4de0b140ec8afd0eeb1d8c4e29342
Steelwall2014 <465964358@qq.com>
2023-05-14 10:49:58 +0800

    Made the header tool be compatible with MSVC

--- src/include/reflection/Macros.h
+++ src/include/reflection/Macros.h
@@ -1,10 +1,22 @@
 #pragma once
 
+#ifdef __clang__
 #define NCLASS __attribute__((annotate("reflect-class")))
+#else
+#define NCLASS
+#endif
 
+#ifdef __clang__
 #define NPROPERTY() __attribute__((annotate("reflect-property")))
+#else
+#define NPROPERTY()
+#endif
 
+#ifdef __clang__
 #define NFUNCTION() __attribute__((annotate("reflect-method")))
+#else
+#define NFUNCTION()
+#endif
 
 #define GENERATE_BODY() \
     private: \
